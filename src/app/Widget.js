7716a6ab30945b3aa141c0c00ec1bdf26222333b
katriyna <katriyna@gmail.com>
2018-06-20 12:18:18 +0300

    added list of users with query-assist

--- src/app/Widget.js
+++ src/app/Widget.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import MultiTable from '@jetbrains/ring-ui/components/table/multitable';
-import Panel from '@jetbrains/ring-ui/components/panel/panel';
-import Button from '@jetbrains/ring-ui/components/button/button';
 import '@jetbrains/ring-ui/components/form/form.scss';
 
+import UserSearch from './UserSearch';
+import SelectedUserForm from './SelectedUserForm';
+
 import styles from './app.css';
-import UserSelect from './UserSelect';
-import UserPropertiesContainer from './UserPropertiesContainer';
-import GroupsTable from './GroupsTable';
-import TeamsTable from './TeamsTable';
-import ProjectRolesTable from './ProjectRolesTable';
-import LoginsTable from './LoginsTable';
-
-const Optional = ({data, children}) => ((data || []).length ? (
-  <div className={styles['property-table']}>{children}</div>
-) : '');
-
-Optional.propTypes = {
-  data: PropTypes.array,
-  children: PropTypes.element
-};
 
 const Widget = (
   {
@@ -34,42 +19,24 @@ const Widget = (
   }
 ) => (
   <div className={styles.widget}>
-    <UserSelect
-      onSelect={onUserSelect}
-      hubService={hubService}
-    />
-
-    {selectedUser && (
-      <div className={styles['user-panel']}>
-        <UserPropertiesContainer/>
-        <div className={styles.userMultiTable}>
-          <MultiTable>
-            <Optional data={selectedUser.groups}>
-              <GroupsTable/>
-            </Optional>
-            <Optional data={selectedUser.teams}>
-              <TeamsTable/>
-            </Optional>
-            <Optional data={selectedUser.projectRoles}>
-              <ProjectRolesTable/>
-            </Optional>
-            <Optional data={selectedUser.details}>
-              <LoginsTable/>
-            </Optional>
-          </MultiTable>
-        </div>
-        <Panel className={styles.widgetFooter}>
-          <Button
-            primary={true}
-            loader={revokingAccess}
-            onClick={onRevokeAccess}
-          >{'Revoke selected items'}</Button>
-          <Button
-            onClick={onCancel}
-          >{'Finish'}</Button>
-        </Panel>
-      </div>
-    )}
+    {
+      selectedUser &&
+      <SelectedUserForm
+        onUserSelect={onUserSelect}
+        selectedUser={selectedUser}
+        onRevokeAccess={onRevokeAccess}
+        onCancel={onCancel}
+        revokingAccess={revokingAccess}
+        hubService={hubService}
+      />
+    }
+    {
+      !selectedUser &&
+      <UserSearch
+        onUserSelect={onUserSelect}
+        hubService={hubService}
+      />
+    }
   </div>
 );
 
