af71ec6a46a2222457b23b6b2772e12ea31e103b
katriyna <katriyna@gmail.com>
2018-09-14 12:35:21 +0300

    map translations keys after po-file is loaded

--- src/app/translations.js
+++ src/app/translations.js
@@ -1,7 +1,13 @@
-import {de} from './translations/revoke-user-access-widget_de.po';
-import {es} from './translations/revoke-user-access-widget_es.po';
-import {fr} from './translations/revoke-user-access-widget_fr.po';
-import {ru} from './translations/revoke-user-access-widget_ru.po';
-import {ja} from './translations/revoke-user-access-widget_ja.po';
+import de from './translations/revoke-user-access-widget_de.po';
+import es from './translations/revoke-user-access-widget_es.po';
+import fr from './translations/revoke-user-access-widget_fr.po';
+import ru from './translations/revoke-user-access-widget_ru.po';
+import ja from './translations/revoke-user-access-widget_ja.po';
 
-export default {de, es, fr, ru, ja};
+export default {
+  de: de.de_DE,
+  es: es.es_ES,
+  fr: fr.fr_FR,
+  ru: ru.ru_RU,
+  ja: ja.ja_JP
+};
