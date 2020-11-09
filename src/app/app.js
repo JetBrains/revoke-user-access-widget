e310abdf0ddc1d1c591513d5037bf52f2c18bb90
katriyna <katriyna@gmail.com>
2020-11-09 21:29:05 +0300

    support custom translations

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,5 @@
 import 'babel-polyfill';
 import DashboardAddons from 'hub-dashboard-addons';
-import {setLocale} from 'hub-dashboard-addons/dist/localization';
 import React from 'react';
 import {Provider} from 'react-redux';
 import {render} from 'react-dom';
@@ -11,11 +10,11 @@ import createStore, {setHubURL} from './ReduxStore';
 
 import WidgetContainer from './WidgetContainer';
 import HubService from './HubService';
-import TRANSLATIONS from './translations';
+import {initTranslations} from './translations';
 
 
 DashboardAddons.registerWidget(dashboardApi => {
-  setLocale(DashboardAddons.locale, TRANSLATIONS);
+  initTranslations(DashboardAddons.locale);
 
   const store = createStore();
   const hubService = new HubService(dashboardApi.fetchHub);
